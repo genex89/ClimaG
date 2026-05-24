ade8c4c43ece632138301a4c6e8680f8e000d0ea
Gex89 <gennaromellone@gmail.com>
2026-05-24 12:32:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+"""
+ClimaG Integration for Home Assistant.
+Developed by: Gex89
+GitHub: https://github.com/genex89
+Description: Deterministic multi-zone climate coordinator.
+"""
+
+
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
