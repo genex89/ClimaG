f6080724c6c5aece4c8d76cda50e1692ed8941d1
Gex89 <gennaromellone@gmail.com>
2026-05-24 12:32:54 +0200

    Update number.py

--- number.py
+++ number.py
@@ -1,3 +1,11 @@
+"""
+ClimaG Integration for Home Assistant.
+Developed by: Gex89
+GitHub: https://github.com/genex89
+Description: Deterministic multi-zone climate coordinator.
+"""
+
+
 from homeassistant.components.number import RestoreNumber
 from homeassistant.const import UnitOfTemperature
 from homeassistant.helpers.entity import EntityCategory
